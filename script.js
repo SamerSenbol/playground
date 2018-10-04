37d4237ed1f088283d79fedfbfcd7dfcf5e73677
Samer <samer.senbol@gmail.com>
2018-10-04 11:48:54 +0200

    Change the color to random color

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
+
+function changeBackground(){
+    var style =document.body.style;
+    if (style.backgroundColor=="white") {
+        
+        style.backgroundColor="purple";        
+    }
+    else{style.backgroundColor="white"}
 console.log("It works!");
+}
+
+function randomizBackground(){
+    var style =document.body.style;
+    var randomValue=Math.random();
+    console.log(randomValue);
+    if(randomValue<0.2){
+        style.backgroundColor="blue";
+    }
+    else if (randomValue<0.4){
+        style.backgroundColor="pink";
+    }
+    else if (randomValue<0.6){
+        style.backgroundColor="orange";
+    }
+  else{  style.backgroundColor="yellow";}
+    }
+
+
+//pass by Value
+//string, number,boolean
+
+//pass by reference
+//arry, object
+
+/* var fotballPlayer={
+    name:"David Jensen",
+    age:22,
+    shirtNumber:14,
+    position:"Mid-fielder",
+    gender:"male"
+
+};
+var newFotballPlayer=fotballPlayer;
+newFotballPlayer.name="Albert Eisteion";
+console.log(newFotballPlayer); 
+console.log(fotballPlayer); */
